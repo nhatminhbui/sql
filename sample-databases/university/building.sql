aa1c96090e791a5714ed73a0c58667f4445e95ea
Nhat Bui <minhnhat@linuxmail.org>
2022-01-09 22:42:26 +0700

    first commit from local

---
+++ sample-databases/university/building.sql
@@ -0,0 +1,8 @@
+INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
+INSERT INTO buildings VALUES ('AAH', 'Center of Computer Science');
+INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
+INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
+INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
+INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
+INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
+INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
