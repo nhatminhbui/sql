0e2e31878f58277c41a4fdf71a6e0fc949977801
Nhat Bui <minhnhat@linuxmail.org>
2022-01-14 15:32:47 +0700

    here we go

--- sample-databases/university/building.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
-INSERT INTO buildings VALUES ('AAH', 'Center of Computer Science');
-INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
-INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
-INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
-INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
-INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
-INSERT INTO buildings VALUES ('CSC', 'Center of Computer Science');
